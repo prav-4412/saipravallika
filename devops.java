9f3848491016e97ad7f96f1dce888c06847a9b77
prav-4412 <kpandu528@gmail.com>
2024-10-22 16:02:03 +0530

    Update devops.java

--- devops.java
+++ devops.java
@@ -2,7 +2,7 @@ public class Hello
 {
      public static void main(String[] args)
        {
-               System.out.print("haiiiiiiiiiiii");
+               System.out.print("haii");
         }
 }
       
